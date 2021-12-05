b4a21f747e41676947704178e6bf749348e01ab1
Victor Winberg <victor.winberg@jayway.com>
2021-12-05 10:42:26 +0100

    refactor day 5

--- src/main/kotlin/days/Day5.kt
+++ src/main/kotlin/days/Day5.kt
@@ -2,7 +2,6 @@ package days
 
 import kotlin.math.abs
 import kotlin.math.max
-import kotlin.math.min
 
 class Day5 : Day(5) {
 
@@ -11,38 +10,28 @@ class Day5 : Day(5) {
     override fun partOne(): Any {
         val input = parseInput()
         val xyInput = input.filter { (a, b) -> a[0] == b[0] || a[1] == b[1] }
-
         return matrixCount(input, xyInput)
     }
 
     override fun partTwo(): Any {
         val input = parseInput()
-        val xyInput = input.filter { (a, b) -> a[0] == b[0] || a[1] == b[1] }
-        val digInput = input.filter { (a, b) -> abs(a[0] - b[0]) == abs(a[1] - b[1]) }
-
-        return matrixCount(input, xyInput, digInput)
+        val xyInput = input.filter { (a, b) -> a[0] == b[0] || a[1] == b[1] || abs(a[0] - b[0]) == abs(a[1] - b[1]) }
+        return matrixCount(input, xyInput)
     }
 
     private fun matrixCount(
         input: List<List<List<Int>>>,
-        xyInput: List<List<List<Int>>>,
-        digInput: List<List<List<Int>>> = listOf()
+        xyInput: List<List<List<Int>>>
     ): Int {
         val size = input.flatten().flatten().maxOrNull() ?: -1
         val matrix = Array(size + 1) { IntArray(size + 1) { 0 } }
         xyInput.forEach { (a, b) ->
-            (min(a[0], b[0])..max(a[0], b[0])).forEach { x ->
-                (min(a[1], b[1])..max(a[1], b[1])).forEach { y ->
-                    matrix[y][x] += 1
-                }
-            }
-        }
-
-        digInput.forEach { (a, b) ->
-            val dx = if (b[0] > a[0]) 1 else -1
-            val dy = if (b[1] > a[1]) 1 else -1
-            (0..abs(a[0] - b[0])).forEach {
-                matrix[a[1] + it * dy][a[0] + it * dx] += 1
+            val dx = b[0] - a[0]
+            val dy = b[1] - a[1]
+            val ix = if (dx > 0) 1 else if (dx < 0) -1 else 0
+            val iy = if (dy > 0) 1 else if (dy < 0) -1 else 0
+            (0..max(abs(dx), abs(dy))).forEach {
+                matrix[a[1] + it * iy][a[0] + it * ix] += 1
             }
         }
 
