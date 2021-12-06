31b88c87d34e9dd68c3cfd6beec37599e61725d1
Victor Winberg <victor.winberg@jayway.com>
2021-12-06 11:28:16 +0100

    refactor day 6

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -3,22 +3,18 @@ package days
 class Day6 : Day(6) {
 
     override fun partOne(): Any {
-        val input = inputString.split(",").map { it.toInt() }.toMutableList()
-
-        (0 until 80).forEach { _ ->
-            val spawn = input.count { it == 0 }
-            input.replaceAll { it - 1 }
-            input.replaceAll { if (it == -1) 6 else it }
-            input.addAll(IntArray(spawn) { 8 }.toList())
-        }
-        return input.size
+        return solve(80)
     }
 
     override fun partTwo(): Any {
+        return solve(256)
+    }
+
+    private fun solve(n: Int): Long {
         val input = inputString.split(",").map { it.toInt() }
         var map = input.groupBy { it }.mapValues { it.value.size.toLong() }.toMutableMap()
 
-        (0 until 256).forEach { _ ->
+        (0 until n).forEach { _ ->
             map = map.mapKeys { it.key - 1 }.toMutableMap()
             val count = map.remove(-1) ?: 0
             map[6] = count + (map[6] ?: 0)
